025169d6119e4215b782dfb94b32180ca0069333
Ravi Veliyat <veliyat@gmail.com>
2018-06-08 13:40:08 +0530

    initiated material ui for admin panel

--- src/app/components/admin/admin.module.ts
+++ src/app/components/admin/admin.module.ts
@@ -1,19 +1,25 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AdminRoutes } from './admin.routes';
+import { AdminMatModule } from './adminmat.module';
 
 import { AdminComponent } from './admin.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { NavComponent } from './shared/nav/nav.component';
 
 @NgModule({
   imports: [
     CommonModule,
-    AdminRoutes
+    BrowserAnimationsModule,
+    AdminRoutes,
+    AdminMatModule
   ],
   declarations: [
     DashboardComponent,
-    AdminComponent
+    AdminComponent,
+    NavComponent    
   ]
 })
 export class AdminModule { }
